a7c60472b185711991a090ec018aca84c1e8aea8
Jacob <46667568+jacobvd0@users.noreply.github.com>
2023-02-23 19:22:55 +1030

    Add files via upload

---
+++ CalculatorThing/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Figgle;
+
+namespace CalculatorThing // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first number");
+            var num1tmp = Console.ReadLine();
+            Console.WriteLine("Enter the second number");
+            var num2tmp = Console.ReadLine();
+            float num1 = Convert.ToInt64(num1tmp);
+            float num2 = Convert.ToInt64(num2tmp);
+
+            //Console.WriteLine(num1 + num2);
+
+            string total = Convert.ToString(num1 + num2);
+            Console.WriteLine(FiggleFonts.Ogre.Render(total));
+        }
+    }
+}
